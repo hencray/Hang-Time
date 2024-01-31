c524ada9e63875222262b564dc9960a3ad363103
hencray <henrymartija@gmail.com>
2024-01-31 21:37:40 +0000

    CI/CD Front-end Prep

--- api/main.py
+++ api/main.py
@@ -35,14 +35,14 @@ def root():
     return {"message": "You hit the root path!"}
 
 
-@app.get("/api/launch-details")
-def launch_details():
-    return {
-        "launch_details": {
-            "module": 3,
-            "week": 17,
-            "day": 5,
-            "hour": 19,
-            "min": "00",
-        }
-    }
+# @app.get("/api/launch-details")
+# def launch_details():
+#     return {
+#         "launch_details": {
+#             "module": 3,
+#             "week": 17,
+#             "day": 5,
+#             "hour": 19,
+#             "min": "00",
+#         }
+#     }
--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,47 +1,52 @@
-import { useEffect, useState } from "react";
-import ErrorNotification from "./ErrorNotification";
+import { BrowserRouter } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import ErrorNotification from "./ErrorNotification";
 import CreateUserForm from "./CreateAccount.js";
 import LoginForm from "./Login.js";
 import Logout from "./Logout.js";
-import { AuthProvider } from './Apiauth'; // Import AuthProvider
+import { AuthProvider } from "./Apiauth"; // Import AuthProvider
 import "./App.css";
 
 function App() {
-  const [launchInfo, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
+  // const [launchInfo, setLaunchInfo] = useState([]);
+  // const [error, setError] = useState(null);
+  const domain = /https:\/\/[^/]+/;
+  const basename = process.env.PUBLIC_URL.replace(domain, "");
 
   const handleLogin = (user) => {
     // Handle login here
     console.log(user);
   };
 
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
-      console.log("fastapi url: ", url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
+  // useEffect(() => {
+  //   async function getData() {
+  //     let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
+  //     console.log("fastapi url: ", url);
+  //     let response = await fetch(url);
+  //     console.log("------- hello? -------");
+  //     let data = await response.json();
 
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, []);
+  //     if (response.ok) {
+  //       console.log("got launch data!");
+  //       setLaunchInfo(data.launch_details);
+  //     } else {
+  //       console.log("drat! something happened");
+  //       setError(data.message);
+  //     }
+  //   }
+  //   getData();
+  // }, []);
 
   return (
-    <AuthProvider> {/* Wrap your application with AuthProvider */}
-      <div>
-        <ErrorNotification error={error} />
-        <CreateUserForm />
-        <LoginForm onLogin={handleLogin} />
-        <Logout onLogout={() => console.log("Logged out")} />
-      </div>
+    <AuthProvider>
+      <BrowserRouter basename={basename}>
+        <div>
+          {/* <ErrorNotification error={error} /> */}
+          <CreateUserForm />
+          <LoginForm onLogin={handleLogin} />
+          <Logout onLogout={() => console.log("Logged out")} />
+        </div>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
