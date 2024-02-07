af4f6f6b086363b87ff6d033eec91e94abf451b0
Christian Ramos <cramos59@gmail.com>
2024-02-07 23:05:48 +0000

    Navigation fixes

--- ghi/src/Events.js
+++ ghi/src/Events.js
@@ -4,14 +4,17 @@ import { useNavigate } from "react-router-dom";
 import "./App.css";
 import ListAttendees from "./ListAttendees";
 import EventsTable from "./EventHistory";
+import { useEffect } from "react";
 
 export const Events = () => {
   const { token } = useToken();
   const navigate = useNavigate();
 
-  if (!token) {
-    navigate("/login");
-  }
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]); // dependencies
 
   return (
     <div>
@@ -19,7 +22,7 @@ export const Events = () => {
       <h1>Event Detail</h1>
       <div className="form-container">
         {token && <ListAttendees />}
-        {token && <EventsTable /> }
+        {token && <EventsTable />}
       </div>
     </div>
   );
